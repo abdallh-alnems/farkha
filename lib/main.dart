6f219386040760b0833defe9e4a74d4eefd4d674
abdallh-alnems <abdallhmoustafa295@gmail.com>
2023-07-05 19:34:24 +0300

    add spalsh screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,10 @@ import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:farkha_app/logic/binding/my_binding.dart';
 import 'package:farkha_app/routes/routes.dart';
 import 'package:farkha_app/view/screen/home/home_screen.dart';
+import 'package:farkha_app/view/widget/splash_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:page_transition/page_transition.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -23,16 +23,7 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       initialBinding: MyBindings(),
-      home: AnimatedSplashScreen(
-        splashIconSize: 90,
-        pageTransitionType: PageTransitionType.leftToRight,
-        splashTransition: SplashTransition.decoratedBoxTransition,
-        
-        splash: Image.asset(
-          'assets/images/logo.png',
-        ),
-        nextScreen: HomeScreen(),
-      ),
+      home: SplashScreen(),
       getPages: AppRoutes.routes,
     );
   }
---
+++ lib/view/widget/splash_screen.dart
@@ -0,0 +1,21 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:farkha_app/view/screen/home/home_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:page_transition/page_transition.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSplashScreen(
+      splashIconSize: 90,
+      pageTransitionType: PageTransitionType.leftToRight,
+      splashTransition: SplashTransition.decoratedBoxTransition,
+      splash: Image.asset(
+        'assets/images/logo.png',
+      ),
+      nextScreen: HomeScreen(),
+    );
+  }
+}
