534876ee15da90a85aa10652564c0797fac12c4d
abdallh-alnems <abdallhmoustafa295@gmail.com>
2023-07-05 07:06:56 +0300

    change logo

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,18 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:farkha_app/logic/binding/my_binding.dart';
 import 'package:farkha_app/routes/routes.dart';
+import 'package:farkha_app/view/screen/home/home_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:page_transition/page_transition.dart';
 
- void main() async {
-
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp();
 
   runApp(const MyApp());
-  
 }
 
 class MyApp extends StatelessWidget {
@@ -21,12 +22,18 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      initialBinding:MyBindings() ,
-      
-      initialRoute: AppRoutes.homeScreen,
+      initialBinding: MyBindings(),
+      home: AnimatedSplashScreen(
+        splashIconSize: 90,
+        pageTransitionType: PageTransitionType.leftToRight,
+        splashTransition: SplashTransition.decoratedBoxTransition,
+        
+        splash: Image.asset(
+          'assets/images/logo.png',
+        ),
+        nextScreen: HomeScreen(),
+      ),
       getPages: AppRoutes.routes,
-      
     );
   }
 }
