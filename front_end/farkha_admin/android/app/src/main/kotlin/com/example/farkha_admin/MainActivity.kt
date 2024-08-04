670ae66a88135f38c6255d560a60af4bc9302b30
abdallh-alnems <abdallhmoustafa295@gmail.com>
2024-08-04 06:16:22 +0300

    first commit

---
+++ front_end/farkha_admin/android/app/src/main/kotlin/com/example/farkha_admin/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.farkha_admin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ front_end/farkha_app/android/app/src/main/kotlin/com/example/farkha_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.farkha_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
